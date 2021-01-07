f0bb40d2bfdaed974b0d2e6e3b826bb975edad9f
Lynda Phan <lynda.kim.phan@gmail.com>
2021-01-07 03:16:07 +0000

    initial commit for OO

--- oo.py
+++ oo.py
@@ -1,1 +1,10 @@
 # Create your classes here
+"""###Part 1###"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        
